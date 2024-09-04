458e936118e8bf725324c8236738e5280abf5931
VadimElFamoso <vadimprofessionel@gmail.com>
2024-09-04 11:51:48 +0200

    started responsive landing page

--- components/landing.vue
+++ components/landing.vue
@@ -10,17 +10,17 @@
       </div>
   
       <!-- Navigation Bar -->
-      <div class="relative flex font-ethnocentric justify-end gap-5 text-white px-5 py-5 z-20">
+      <div class="relative flex font-ethnocentric justify-end gap-5 text-lg text-white px-5 py-5 z-20">
         <img class="absolute h-auto left-5 top-5 p-2 w-1/6" src="../public/codecorp_logo.png" alt="">
-        <a href="#" class="underline p-2">Services</a>
-        <a href="#" class="underline p-2">Projets</a>
-        <a href="#" class="underline p-2">A propos</a>
-        <a href="#" class="underline p-2">Blog</a>
+        <a href="#" class="hover:text-primary underline p-2 transition">Services</a>
+        <a href="#" class="hover:text-primary underline p-2 transition">Projets</a>
+        <a href="#" class="hover:text-primary underline p-2 transition">A propos</a>
+        <a href="#" class="hover:text-primary underline p-2 transition">Blog</a>
         <a href="#" class="text-primary border-2 border-primary border-solid p-2">Nous contacter</a>
       </div>
   
       <!-- Main Content -->
-      <div class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 font-ethnocentric font-bold text-white text-6xl w-10/12 z-10">
+      <div class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 font-ethnocentric font-bold text-tertiary text-4xl lg:text-6xl w-10/12 z-10">
         <p>Nous façonnons l'architecture, le paysage et l'intérieur du web moderne.</p>
         <a href=""></a>
       </div>
