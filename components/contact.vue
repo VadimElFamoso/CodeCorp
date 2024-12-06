ddd9b44be8e6ae1d69f0c1b5f6958ed9bd492c20
VadimElFamoso <vadimprofessionel@gmail.com>
2024-12-06 20:27:13 +0100

    contact form beta

--- components/contact.vue
+++ components/contact.vue
@@ -14,32 +14,61 @@
       </div>
 
       <!--Contact form-->
-      <div class="contact_form h-auto w-[60vw]">
+      <div class="contact_form h-auto w-[60vw] text-tertiary">
         <div class="bg-primary border-tertiary m-auto font-ethnocentric font-bold my-10 rotate-3 text-center text-secondary text-3xl p-4 w-10/12">
         <h2>Propulsez votre vision <br>dans le futur</h2>
         </div>
 
-        <form action="" class="font-areaextended m-auto w-10/12">
-          <p for="" class="text-tertiary">Votre nom :</p>
-          <input type="text">
+        <form action="" class="font-areaextended flex flex-col gap-10 m-auto w-10/12">
 
-          <p for="" class="text-tertiary">Votre email :</p>
-          <input type="text">
+          <!--Name container-->
+          <div class="flex gap-10">
 
-          <p for="" class="text-tertiary">Sujet :</p>
-          <select name="" id="">
-            <option value="">Solicitation d'une offre</option>
-            <option value="">Prise de contact préalable pour devis</option>
-            <option value="">Networking BTB</option>
-            <option value="">Autre</option>
-          </select>
+            <div class="flex flex-col basis-1/2">
+              <p for="" class="text-tertiary">Votre nom :</p>
+              <input class="border-b-2 justify-between border-tertiary bg-secondary pt-1 pb-1" type="text">
+            </div>
 
-          <p for="" class="text-tertiary">Message :</p>
-          <textarea name="" id="" cols="100" rows="10"></textarea>
+            <div class="flex flex-col basis-1/2">
+              <p for="" class="text-tertiary">Votre email :</p>
+              <input class="border-b-2 border-tertiary bg-secondary pt-1 pb-1" type="text">
+            </div>
+
+          </div>
+
+          <!--Details container-->
+          <div class="flex gap-10">
+
+            <div class="flex flex-col basis-1/2">
+              <p for="" class="text-tertiary">Votre numéro de téléphone :</p>
+              <input class="border-b-2 justify-between border-tertiary bg-secondary pt-1 pb-1" type="text">
+            </div>
+
+            <div class="flex flex-col basis-1/2">
+              <p for="" class="text-tertiary">Votre raison sociale (optionnel) :</p>
+              <input class="border-b-2 border-tertiary bg-secondary pt-1 pb-1" type="text">
+            </div>
+          </div>
+
+          <!--Select-->
+          <div>
+            <p for="" class="text-tertiary">Sujet :</p>
+            <select class="border-b-2 border-tertiary bg-secondary pt-1 pb-1 w-full" name="" id="">
+              <option value="">Solicitation d'une offre</option>
+              <option value="">Prise de contact préalable pour devis</option>
+              <option value="">Networking BTB</option>
+              <option value="">Autre</option>
+            </select>
+          </div>
+
+          <div>
+            <p>Message :</p>
+            <textarea class="border-b-2 border-tertiary bg-secondary pt-1 pb-1 h-auto w-full" name="" id="" cols="30" rows="5"></textarea>
+          </div>
         </form>
 
         <!--Div après envoi du message-->
-        <div class="text-primary">
+        <div class="text-primary hidden">
           <p>Votre message a bien été envoyé.</p>
         </div>
       </div>
